785131c624f0154a223f087f9eec636e67029df4
Aneesh Maganti <28660350+aminoa@users.noreply.github.com>
2023-10-15 17:27:21 -0400

    wip on memory controller reads

--- other/generate-nums.py
+++ other/generate-nums.py
@@ -4,18 +4,20 @@ import random
 number_list = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768]
 
 # Initialize the result list with 0s
-result_list = [0] * 5004
+result_list = [0] * 1004
 
-# Generate 5004 bytes in groups of 12
-for i in range(0, len(result_list), 12):
-    random_number = random.choice(number_list)
-    random_index = random.randint(0, 11)
-    result_list[i + random_index] = random_number
+# Generate 5004 bytes in groups of 16
+for i in range(0, len(result_list) - 16, 16):
+    # random_numbers = random.sample(number_list, 3)  # Choose three random numbers
+    random_indices = random.sample(range(16), 3)  # Choose three random indices within the group
+    print(random_indices)
+    for j in range(3):
+        result_list[i + random_indices[j]] = number_list[random_indices[j]]
+        print(result_list[i + random_indices[j]])
 
 # Print the result list (first 60 bytes as an example)
-print(result_list[:60])
+# print(result_list[:60])
 
-# write this list to a file called "numbers.txt" in a list format
+# write this to a file
 with open("numbers.txt", "w") as f:
     f.write(str(result_list))
-    
